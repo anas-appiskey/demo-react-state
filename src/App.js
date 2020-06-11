924acb05b56d190cccd6782b22594c4d14fc7411
anasali2193 <anasali2193@gmail.com>
2020-06-11 09:58:18 +0500

    add temperate assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import Room from './Room';
 
 function App() {
   return (
-    <h1 className="App">
+    <div className="App-header">
       <Room></Room>
-    </h1>
+    </div>
   );
 }
 
--- src/Room.js
+++ src/Room.js
@@ -1,9 +1,11 @@
 import React,{useState} from 'react';
+import './Room.css';
 
 
 function Room() {
     let [isLit, setLit] = useState(true);
-    let [Age, setAge] = useState(21);
+    // let [Age, setAge] = useState(21);
+    let [temp, setTemp] = useState(22);
 
     // function updateList(){
     //     // console.log('button clicked')
@@ -13,20 +15,37 @@ function Room() {
 
     
   return (
-    <div>
-        This is Room  : {isLit?'lit' :'dark'}
-        <br />
-        Age is   : {Age}
+    <div className={`room ${isLit ? 'lit': 'dark'} `}>
+    {/* <div className='room'> */}
+        <div>
+            This Room switch is  : {isLit?'On' :'Off'}
+            <br />
+            {/* Age is   : {Age} */}
 
-        <br />
-        <button onClick={()=>{setLit(!isLit)}}>toggle light</button>
+            <br />
+            Room Temperature   : {temp} Celsius
 
-        <br />
-        <button onClick={()=>{
-            setAge(++Age);
-        }}>
-        Increase Age</button>
+            <br />
+        </div>
+        <div>
+            <button onClick={()=>{setLit(true)}}>light On</button>
 
+            <button onClick={()=>{setLit(false)}}>light Off</button>
+
+
+            <br />
+
+            <button onClick={()=>{
+                setTemp(++temp);
+            }}>
+            Increase Temperature</button>
+
+            <br />
+            <button onClick={()=>{
+                setTemp(--temp);
+            }}>
+            Increase Temperature</button>
+        </div>
     </div>
   );
 }
